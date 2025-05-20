a65f649991aab3bb31414f7b9329c74ee0bb2f0d
Abd766 <abdullahmasood766@gmail.com>
2025-05-20 13:33:28 +0530

    Resolved CORS Error

--- Backend/index.js
+++ Backend/index.js
@@ -9,13 +9,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors(
-  {
-    origin: [process.env.FRONTEND_DEV, process.env.FRONTEND],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  }
-));
-
+// app.use(cors(
+//   {
+//     origin: [process.env.FRONTEND_DEV, process.env.FRONTEND],
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   }
+// ));
+app.use(cors());
 // Available Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
